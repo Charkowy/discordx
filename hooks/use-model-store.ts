89831af6e57a17660f287b90449fe9a84c1f3b1f
angieVentura <angiev.d.23@gmail.com>
2024-10-19 21:08:41 -0300

    Agrego channels

--- hooks/use-model-store.ts
+++ hooks/use-model-store.ts
@@ -1,10 +1,12 @@
+import { ChannelType } from "@prisma/client";
 import { Server } from "http";
-import {create} from "zustand";
+import { create } from "zustand";
 
 export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer";
 
-interface ModalData{
-    server?: Server
+interface ModalData {
+    server?: Server;
+    channelType?: ChannelType;
 }
 
 interface ModalStore {
@@ -13,13 +15,13 @@ interface ModalStore {
     isOpen: boolean;
     onOpen: (type: ModalType, data?: ModalData) => void;
     onClose: () => void;
-} 
+}
 
 export const useModal = create<ModalStore>((set) => ({
     type: null,
-    data: {}, 
+    data: {},
     isOpen: false,
-    onOpen: (type, data = {})  => set({ isOpen: true, type, data }),
+    onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
     onClose: () => set({ type: null, isOpen: false })
 }));
 
