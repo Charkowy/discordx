332fc1d30258f207d5f99f9bf6a3e2470ce6b3de
angieVentura <angiev.d.23@gmail.com>
2024-10-25 23:38:15 -0300

    Agrego funcionalidad al chat, arreglo navegabilidad entre servers y arreglo estilos de lo sidebar

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -34,7 +34,7 @@ export const ServerMember = ({
         <button
             onClick={onClick}
             className={cn(
-                "group px-22 py-2 rounded-md item-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+                "group px-22 py-2 rounded-md items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1 flex flex-row justify-center",
                 params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
             )}
         >
